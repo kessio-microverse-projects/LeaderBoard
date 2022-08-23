540a769246fcaa0d22a8b949fe6b40b388c942b0
kessio <64524758+kessio@users.noreply.github.com>
2022-08-23 13:58:07 +0300

    Update readme and fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-import _ from 'lodash';
 import './styles/style.css';
-import renderTable from './modules/render-table.js'; 
+import renderTable from './modules/render-table.js';
 
 renderTable();
-
-const tbEvenRows = document.querySelector("#table-data tr:nth-child(2)");
-console.log(tbEvenRows);
--- src/modules/render-table.js
+++ src/modules/render-table.js
@@ -1,41 +1,37 @@
 const renderTable = () => {
-    const tableData = [
-        {
-        name:'Name:',
-        score: 100
+  const tableData = [
+    {
+      name: 'Name:',
+      score: 100,
     },
     {
-        name:'Name:',
-        score: 20
+      name: 'Name:',
+      score: 20,
     },
     {
-        name:'Name:',
-        score: 50
+      name: 'Name:',
+      score: 50,
     },
     {
-        name:'Name:',
-        score: 78
+      name: 'Name:',
+      score: 78,
     },
     {
-        name:'Name:',
-        score: 125
+      name: 'Name:',
+      score: 125,
     },
     {
-        name:'Name:',
-        score: 77
-    }  
-    ];
+      name: 'Name:',
+      score: 77,
+    },
+  ];
 
-    tableData.forEach(data => {
-        const tableContainer = document.querySelector('.table-body');
-        tableContainer.innerHTML += `
+  tableData.forEach((data) => {
+    const tableContainer = document.querySelector('.table-body');
+    tableContainer.innerHTML += `
                     <tr>
                     <td>${data.name}${data.score}</td>
-                    </tr> `;                 
-    });
-    
-}
+                    </tr> `;
+  });
+};
 export default renderTable;
-
-
-
